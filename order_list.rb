010d422da2c3c71aa228692a8c2bbe3efa4e6f8f
sergejapetenok <exorich@yandex.ru>
2020-02-28 15:47:28 +0400

    Initial commit

---
+++ order_list.rb
@@ -0,0 +1,12 @@
+to_order = ["масло", "молоко", "хлопья", "картошка", "курица", "помидоры"]  # Массив с списками покупок
+while to_order.size > 0 do    # Цикл который удаляет из массива элемент если он совпадает с полученной из gets покупкой
+  puts "Нужно еще купить:"
+  puts to_order.to_s
+  choice = gets.chomp.to_s
+  if to_order.include? (choice)  # Если возвращаем true то просто удаляем запись из массива что сделает счетчик to_order.size меньше
+    to_order.delete (choice)
+  else
+    puts choice + " - это спонтанная покупка"
+  end
+  end
+puts "Вы все купили"
